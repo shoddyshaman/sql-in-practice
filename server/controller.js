7635668455725722c865558c9abecf27986af4ba
shoddyshaman <dhananjoy.tiwari@gmail.com>
2022-07-20 11:34:56 -0600

    added seed data

---
+++ server/controller.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const { DATABASE_URL } = process.env;
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  }
+});
+
+module.exports = {
+    getUserInfo: (req,res) => {
+        
+    }
+
+}
